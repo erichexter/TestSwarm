b628f75212ec56df5206b2fca1533ff39aef93b9
Eric Hexter <eric.hexter@gmail.com>
2013-01-23 15:54:22 -0600

    updated assembly name for migrations

--- src/ci-database.cmd
+++ src/ci-database.cmd
@@ -1,1 +1,1 @@
-packages\EntityFramework.6.0.0-alpha2\tools\migrate.exe /Verbose runtime /ConnectionString="Data Source=dev-testswarm.oldev.preol.dell.com\sqlexpress;Initial Catalog=ntestswarm_dev;Integrated Security=SSPI" /ConnectionProviderName="System.Data.SqlClient"  /startupdirectory=www\bin
+packages\EntityFramework.6.0.0-alpha2\tools\migrate.exe /Verbose www /ConnectionString="Data Source=dev-testswarm.oldev.preol.dell.com\sqlexpress;Initial Catalog=ntestswarm_dev;Integrated Security=SSPI" /ConnectionProviderName="System.Data.SqlClient"  /startupdirectory=www\bin
