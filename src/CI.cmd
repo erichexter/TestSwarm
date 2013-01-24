e1839c6aa4b6cadcce1603d96a838286c02a2b8e
Eric Hexter <eric.hexter@gmail.com>
2013-01-24 11:34:56 -0600

    fixed config

--- src/CI.cmd
+++ src/CI.cmd
@@ -1,3 +1,5 @@
 xcopy /s  /y www \\dev-testswarm\dev
+echo transform config
 packages\WebConfigTransformRunner.1.0.0.1\Tools\WebConfigTransformRunner.exe www\Web.config www\Web.ci.config \\dev-testswarm\dev\web.config
+echo migrating database
 ci-database.cmd
