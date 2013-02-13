4695f58d8d8cbd5e8108659c17d0b8164451897b
Jason Reimer <jareimer1@gmail.com>
2013-02-13 10:46:27 -0600

    added debug statements

--- src/www/Hubs/LastJobStatusHub.cs
+++ src/www/Hubs/LastJobStatusHub.cs
@@ -1,11 +1,18 @@
 ﻿using Microsoft.AspNet.SignalR;
+using System.Diagnostics;
 
 namespace nTestSwarm.Hubs
 {
     public class LastJobStatusHub : Hub
     {
+        public LastJobStatusHub()
+        {
+            Debug.WriteLine("Hub created.");
+        }
+
         public void Connect()
         {
+            Debug.WriteLine("Connected to id {0}", (object)Context.ConnectionId);
         }
     }
 }
