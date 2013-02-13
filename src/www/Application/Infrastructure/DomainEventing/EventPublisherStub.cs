59710f7a3fe60ca36b8e18ef7a6fd5c01cccd988
Jason Reimer <jareimer1@gmail.com>
2013-02-13 07:30:59 -0600

    Added hub call

--- src/www/Application/Infrastructure/DomainEventing/EventPublisherStub.cs
+++ src/www/Application/Infrastructure/DomainEventing/EventPublisherStub.cs
@@ -1,5 +1,7 @@
-﻿using nTestSwarm.Application.Events.JobCompletion;
+﻿using Microsoft.AspNet.SignalR;
+using nTestSwarm.Application.Events.JobCompletion;
 using nTestSwarm.Application.Infrastructure.Threading;
+using nTestSwarm.Hubs;
 
 namespace nTestSwarm.Application.Infrastructure.DomainEventing
 {
@@ -17,11 +19,12 @@ namespace nTestSwarm.Application.Infrastructure.DomainEventing
             _threadPool.Queue(() => HandleEvent(@event));
         }
 
+        // temporary impl
         private void HandleEvent<TEvent>(TEvent @event) where TEvent : class, IDomainEvent
         {
             if (@event is RunCompleted)
             {
-
+                GlobalHost.ConnectionManager.GetHubContext<LastJobStatusHub>().Clients.All.statusUpdated(@event);
             }
         }
     }
