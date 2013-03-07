17689cb16217c2c6339b4598505df486321c50bb
Jason Reimer <jareimer1@gmail.com>
2013-03-07 13:05:27 -0600

    completed impl

--- src/www/Api/NeededClientsController.cs
+++ src/www/Api/NeededClientsController.cs
@@ -1,13 +1,35 @@
-﻿using nTestSwarm.Areas.Api.Controllers;
-using System.Web.Http;
+﻿using nTestSwarm.Application.Infrastructure.BusInfrastructure;
+using nTestSwarm.Areas.Api.Controllers;
+using nTestSwarm.Areas.Utils.Controllers;
+using System.Linq;
 
 namespace nTestSwarm.Api
 {
-    public class NeededClientsController : ApiController
+    public class NeededClientsController : BusControllerBase
     {
+        public NeededClientsController(IBus bus) : base(bus)
+        {
+        }
+
         public NeededClientResults Get()
         {
-            return null;
+            var result = HandleRequest(new RunDiagnosticsQuery());
+            var response = new NeededClientResults
+            {
+                UserAgents = result
+                                .Where(d => string.IsNullOrWhiteSpace(d.IpAddress))
+                                .Select(r => new NeededClient
+                                {
+                                    Browser = r.UserAgent,
+                                    Version = r.UserAgentVersion,
+                                }).Distinct().ToList(),
+
+                Jobs = result.Select(e => e.JobId).Distinct().ToList(),
+
+                ClientUrl = Url.Link(WebApiConfig.DefaultRoute, new { controller = "clients" })
+            };
+
+            return response;
         }
     }
 }
