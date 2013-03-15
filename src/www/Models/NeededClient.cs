83ea230797c437df102743ca89e957d2b3e6c915
Jason Reimer <jareimer1@gmail.com>
2013-03-15 15:23:05 -0500

    impl IEquatable for Distinct compare

--- src/www/Models/NeededClient.cs
+++ src/www/Models/NeededClient.cs
@@ -1,9 +1,33 @@
-﻿namespace nTestSwarm.Models
+﻿using System;
+
+namespace nTestSwarm.Models
 {
-    public class NeededClient
+    public class NeededClient : IEquatable<NeededClient>
     {
         public string Browser { get; set; }
 
         public int? Version { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as NeededClient);
+        }
+
+        public bool Equals(NeededClient other)
+        {
+            if (other == null) return false;
+
+            if (object.ReferenceEquals(this, other)) return true;
+
+            return Browser == other.Browser && Version == other.Version;
+        }
+
+        public override int GetHashCode()
+        {
+            int browserHash = Browser == null ? 0 : Browser.GetHashCode();
+            int versionHash = Version.HasValue ? Version.GetHashCode() : 0;
+
+            return browserHash ^ versionHash;
+        }
     }
 }
