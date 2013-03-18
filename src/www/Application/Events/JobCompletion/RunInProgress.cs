d7d25c3fa7f99198539c6ccb2bdcf8153bca22bd
Jason Reimer <jareimer1@gmail.com>
2013-03-18 09:47:01 -0500

    added parameterless ctor

--- src/www/Application/Events/JobCompletion/RunInProgress.cs
+++ src/www/Application/Events/JobCompletion/RunInProgress.cs
@@ -4,13 +4,17 @@ namespace nTestSwarm.Application.Events.JobCompletion
 {
     public class RunInProgress : IDomainEvent
     {
+        public RunInProgress()
+        {
+        }
+
         public RunInProgress(long jobId, long runId)
         {
             JobId = JobId;
             RunId = runId;
         }
 
-        public long JobId { get; private set; }
-        public long RunId { get; private set; }
+        public long JobId { get; set; }
+        public long RunId { get; set; }
     }
 }
