4dac76918a07495715aa25435d235751b614aa26
Jason Reimer <jareimer1@gmail.com>
2013-03-18 08:52:52 -0500

    added JobCreated event

---
+++ src/www/Application/Events/JobCreated.cs
@@ -0,0 +1,14 @@
+﻿using nTestSwarm.Application.Infrastructure.DomainEventing;
+
+namespace nTestSwarm.Application.Events
+{
+    public class JobCreated : IDomainEvent
+    {
+        public JobCreated(long jobId)
+        {
+            JobId = jobId;
+        }
+
+        public long JobId { get; private set; }
+    }
+}
---
+++ src/www/Application/Events/JobCreatedStatusNotifier.cs
@@ -0,0 +1,13 @@
+﻿using nTestSwarm.Application.Infrastructure.BusInfrastructure;
+using nTestSwarm.Hubs;
+
+namespace nTestSwarm.Application.Events
+{
+    public class JobCreatedStatusNotifier : IHandler<JobCreated>
+    {
+        public void Handle(JobCreated message)
+        {
+            JobStatusHub.JobStarted(message.JobId);
+        }
+    }
+}
