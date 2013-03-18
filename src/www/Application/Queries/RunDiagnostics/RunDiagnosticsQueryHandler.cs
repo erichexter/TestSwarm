97169e506028e0785616fef29217e44c3bb09c8c
Jason Reimer <jareimer1@gmail.com>
2013-03-18 10:00:08 -0500

    cleanup - removed unneeded code

--- src/www/Application/Queries/RunDiagnostics/RunDiagnosticsQueryHandler.cs
+++ src/www/Application/Queries/RunDiagnostics/RunDiagnosticsQueryHandler.cs
@@ -17,46 +17,28 @@ namespace nTestSwarm.Application.Queries.RunDiagnostics
 
         public IEnumerable<RunDiagnosticsResult> Handle(RunDiagnosticsQuery request)
         {
-            var queryResult = from r in _db.RunUserAgents
-                              join c in _db.Clients on r.ActiveClientId equals c.Id into clients
-                              let client = clients.FirstOrDefault()
-                              let ip = client == null ? string.Empty : client.IpAddress
-                              let ua = r.UserAgent.Name
-                              let uv = r.UserAgent.Version
-                              let os = client == null ? string.Empty : client.OperatingSystem
-                              where r.Status == (int)RunStatusType.Running
-                              orderby r.Updated descending
-                              select new
-                              {
-                                  RunName = r.Run.Name,
-                                  JobName = r.Run.Job.Name,
-                                  JobId = r.Run.Job.Id,
-                                  IpAddress = ip,
-                                  UserAgent = ua,
-                                  UserAgentVersion = uv,
-                                  OperatingSystem = os,
-                                  r.Updated
-                              };
+            var query = from r in _db.RunUserAgents
+                        join c in _db.Clients on r.ActiveClientId equals c.Id into clients
+                        let client = clients.FirstOrDefault()
+                        let ip = client == null ? string.Empty : client.IpAddress
+                        let ua = r.UserAgent.Name
+                        let uv = r.UserAgent.Version
+                        let os = client == null ? string.Empty : client.OperatingSystem
+                        where r.Status == (int)RunStatusType.Running
+                        orderby r.Updated descending
+                        select new RunDiagnosticsResult
+                        {
+                            RunName = r.Run.Name,
+                            JobName = r.Run.Job.Name,
+                            JobId = r.Run.Job.Id,
+                            IpAddress = ip,
+                            UserAgent = ua,
+                            UserAgentVersion = uv,
+                            OperatingSystem = os,
+                            Updated = r.Updated
+                        };
 
-            var array = queryResult.ToArray();
-
-            if (!array.Any())
-            {
-                return new RunDiagnosticsResult[0];
-            }
-
-            return array.Select(x => new RunDiagnosticsResult
-            {
-                RunName = x.RunName,
-                JobName = x.JobName,
-                JobId = x.JobId,
-                IpAddress = x.IpAddress,
-                UserAgent = x.UserAgent,
-                UserAgentVersion = x.UserAgentVersion,
-                OperatingSystem = x.OperatingSystem,
-                Updated = x.Updated,
-            }).ToArray();
+            return query.ToArray();
         }
     }
-
 }
