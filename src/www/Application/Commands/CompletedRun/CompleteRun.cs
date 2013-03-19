907a94f9b5cbbe88b945eb82fa89c4cea15d9469
Jason Reimer <jareimer1@gmail.com>
2013-03-19 09:02:09 -0500

    odd breakage in value types

--- src/www/Application/Commands/CompletedRun/CompleteRun.cs
+++ src/www/Application/Commands/CompletedRun/CompleteRun.cs
@@ -3,12 +3,8 @@ namespace nTestSwarm.Application.Commands.CompletedRun
 {
     public class CompleteRun
     {
-        [Required]
         public long RunId { get; set; }
-
-        [Required]
         public long ClientId { get; set; }
-
         public int Fail { get; set; }
         public int Error { get; set; }
         public int Total { get; set; }
